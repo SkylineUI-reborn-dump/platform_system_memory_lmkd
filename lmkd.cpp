c8ee52bdc2f8c4e57eb6a1d10166c051144b92bc
Divyanand Rangu <quic_drangu@quicinc.com>
2024-10-28 15:36:17 +0530

    lmkd: fix higher event being reset by lower and polling start
    This patch addresses below 2 problems:
    
    Problem-1: When both Medium and Critical events occur at same
    time, depending on how the events are queued, the later event
    resets the former event.
    We'd want the subsequent polling (till next event is triggered)
    to happen with higher event.
    So, it is fine if Critical event overrides Medium, but not other
    way around.
    
    Let's see below scenario where both Medium and Critical events
    occur (at T0) and handled one after other
    T0: critical event handled.
    T0 + 2ms: medium event handled.
    T0 + 102ms: medium event polling check. //This should be critical poll
    
    Problem-2:
    When a new event occurs (could be of same level or different,
    doesn't matter) after a event-poll is handled, poll of new event
    waits only for "100ms - time since last poll is handled".
    But new poll should start after 100ms of last triggered event.
    T0: event-1 triggered.
    T100: event-1 polled
    T120: event-2 triggered.
    T200: event-2 polled. (This poll should happen 100ms after T120, i.e., T220)
    
    Bug: 376003899
    Change-Id: I16ff3b999d7531435324a628ac17968fd4cae8cf

--- lmkd.cpp
+++ lmkd.cpp
@@ -211,6 +211,8 @@ static int mpevfd[VMPRESS_LEVEL_COUNT] = { -1, -1, -1 };
 static bool pidfd_supported;
 static int last_kill_pid_or_fd = -1;
 static struct timespec last_kill_tm;
+static struct timespec prev_trigger_tm;
+enum vmpressure_level prev_level = VMPRESS_LEVEL_LOW;
 static bool monitors_initialized;
 static bool boot_completed_handled = false;
 
@@ -2731,6 +2733,22 @@ static void mp_event_psi(int data, uint32_t events, struct polling_params *poll_
         return;
     }
 
+    if (events > 0 ) {
+        /* We got a lower event. Ensure that it is a geniune event
+         * and not just a queued up event waiting to be handled.
+         */
+        long time_diff_ms = get_time_diff_ms(&prev_trigger_tm, &curr_tm);
+        if (level < prev_level && time_diff_ms < PSI_POLL_PERIOD_LONG_MS) {
+            // event occured too soon. Continue with previous higher event.
+            ALOGD("Ignoring %s pressure event; occured too soon.",
+                       level_name[level]);
+            return;
+        }
+        prev_level = level;
+        prev_trigger_tm.tv_sec  = curr_tm.tv_sec;
+        prev_trigger_tm.tv_nsec = curr_tm.tv_nsec;
+    }
+
     record_wakeup_time(&curr_tm, events ? Event : Polling, &wi);
 
     bool kill_pending = is_kill_pending();
@@ -3825,6 +3843,7 @@ static void call_handler(struct event_handler_info* handler_info,
          */
         poll_params->poll_start_tm = curr_tm;
         poll_params->poll_handler = handler_info;
+        poll_params->last_poll_tm = curr_tm;
         break;
     case POLLING_PAUSE:
         poll_params->paused_handler = handler_info;
